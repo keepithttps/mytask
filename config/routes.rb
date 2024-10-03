f42cef9ea9e71afb9fa8d3da1da2fdf7213df7de
to_hoon <m911nn@gmail.com>
2024-10-04 00:15:47 +0200

    Page_Author

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'about/author', to: 'pages#author'
+>>>>>>> fd87247 (Page_Author)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
